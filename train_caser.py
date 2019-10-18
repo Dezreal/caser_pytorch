fdec1c0ee92a180ecfccd8518a5e0fb6e9b68ed4
meowch <1311787255@qq.com>
2019-10-18 23:24:13 +0800

    -_-

--- train_caser.py
+++ train_caser.py
@@ -143,7 +143,7 @@ class Recommender(object):
                                                     negatives.to(self._device))
 
             epoch_loss = 0.0
-            #self._net.load_state_dict(torch.load('state_dict', map_location=torch.device('cpu')))
+            # self._net.load_state_dict(torch.load('state_dict', map_location=torch.device('cpu')))
 
             for (minibatch_num,
                  (batch_users,
@@ -204,7 +204,7 @@ class Recommender(object):
                                                                         time() - t2)
                 print(output_str)
 
-        torch.save(self._net.state_dict(), "state_dict")
+        # torch.save(self._net.state_dict(), "state_dict")
 
     def _generate_negative_samples(self, users, interactions, n):
         """
@@ -292,14 +292,14 @@ if __name__ == '__main__':
     parser.add_argument('--L', type=int, default=5)
     parser.add_argument('--T', type=int, default=3)
     # train arguments
-    parser.add_argument('--n_iter', type=int, default=8)
+    parser.add_argument('--n_iter', type=int, default=20)
     parser.add_argument('--seed', type=int, default=1234)
     parser.add_argument('--batch_size', type=int, default=512)
     parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--l2', type=float, default=1e-6)
     parser.add_argument('--neg_samples', type=int, default=3)
     parser.add_argument('--use_cuda', type=str2bool, default=False)
-    parser.add_argument('--eval_per_epoch', type=int, default=3)
+    parser.add_argument('--eval_per_epoch', type=int, default=4)
 
     config = parser.parse_args()
 
